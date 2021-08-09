fb315b1ce12f3732e718313e625822c1aa39d200
Orignal42 <rbarriquand@gmail.com>
2021-08-09 11:40:59 +0200

    premier

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+
+const App = () => (
+    <div className="App">
+      <Monster />
+      <br></br>
+      <section className="container-fluid">
+        <PlayerList />
+      </section>
+    </div>
+  )
+
+  
+  export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+// créer ce fichier dans src/reducers/index.js
+ 
+const initialState = {
+    //TODO : complete players {} and monster{}
+    players: {},
+    monster: {}
+  };
+   
+  function rootReducer(state = initialState, action) {
+    return state;
+  };
+   
+  export default rootReducer;
